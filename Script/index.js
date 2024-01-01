5ed3f5f666fa4596f5dcd5561ee6a97cf8b3fc4f
Johannes Thorén <johannes@lgjt.xyz>
2024-01-01 18:30:58 +0100

    poof

--- Script/index.js
+++ Script/index.js
@@ -1,38 +1,37 @@
-const Social = (img, alt, link) => {return A("",link).Child(Img(img, alt))}
+const Header = (headerText) => {
+  return Div(
+    H(headerText).Id("header-text"),
+    Ul(
+      Li(RouterLink("Home", "/")).Class("nav-item"),
+      // Li(RouterLink("About", "/About")).Class("nav-item"),
+      // Li(RouterLink("Repositories", "/Repos")).Class("nav-item")
+    ).Id("nav")
+  ).Id("header")
+}
 
-const Home = () => {return Tab("Home", Div(
-  H("Welcome!", 2),
-  P("Welocme to my website! Here you will find everything from project to information about me. Feel free to explore. Keep In mind that this page is still under construction.")
-).Class("ContentWrapper"))}
-
-const Projects = () => {
-    let list = Ul().OnAppend(async()=>{
-      let response = await fetch("https://api.github.com/users/JohannesThoren/repos?per_page=1000")
-      let data = await response.json()
-
-      data.forEach((el) => {
-        let name = el.fork ? "Fork of " + el.name : el.name
-        list.Child(Li(
-          A(name, el.html_url), 
-          el.description ? P(el.description) : P("No Description"),
-        ))
-      })
-    })
-  
-    return Tab("Projects", Div(
-      H("Projects", 2),
-      P("Here are some of my projects that are avalible on github"),
-      Tag("hr"),
-      list
-    ).Class("ContentWrapper"))
+const Page = (...children) => {
+  let body = Div()
+  children.forEach((c) => body.Child(c))
+  return body
 }
 
 
-OnLoad(
-  Div(
-    H("LGJT.XYZ", 1)).Id("header"),
-  TabRoot(
-    Home(),
-    Projects()
+
+
+RouterOnLoad(
+  Router(
+    Route("/", Page(
+      Header("LGJT.XYZ"),
+      Div(
+        P("Hi, Glad to see that you have found my webpage :)"),
+        P("I am sorry to tell you tho that this page is under construction, feel free to comeback later!")
+      ).Id("content")
+    ).Id("Home")),
+    // Route("/About", Page(
+    //   Header("About")
+    // ).Id("About")),
+    // Route("/Repos", Page(
+    //   Header("Repositories")
+    // ).Id("Repos"))
   )
 )
