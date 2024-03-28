d260df1fb7781d5eb83b2facc35eac42e4ced238
Johannes Thorén <45944438+JohannesThoren@users.noreply.github.com>
2024-03-28 22:01:35 +0000

    Added some js that generates the nav and footer

---
+++ scripts/AddFooter.js
@@ -0,0 +1,5 @@
+function AddFooter() {
+  const footer = document.createElement("footer");
+  footer.innerHTML = `Copyright (C) 2024 Johannes Thorén`;
+  document.body.appendChild(footer);
+}
---
+++ scripts/AddNav.js
@@ -0,0 +1,14 @@
+function AddNav() {
+  // Create the nav element
+  const nav = document.createElement("nav");
+  nav.innerHTML = `
+    <ul>
+      <li><a href="contact.html">Contact</a></li>
+      <li><a href="about.html">About</a></li>
+      <li><a href="https://github.com/JohannesThoren">Github</a></li>
+      <li><a href="https://www.instagram.com/johannes.thoren/">Instagram</a></li>
+    </ul>
+    `
+  // Append the nav element to the body
+  document.querySelector("Header").appendChild(nav)
+}
