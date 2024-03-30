2c0c650495a05cae1f7f220d74e37cf79b6fc9ba
Johannes Thorén <45944438+JohannesThoren@users.noreply.github.com>
2024-03-30 18:03:48 +0000

    Added CV

--- scripts/AddNav.js
+++ scripts/AddNav.js
@@ -5,6 +5,7 @@ function AddNav() {
     <ul>
       <li><a href="contact.html">Contact</a></li>
       <li><a href="about.html">About</a></li>
+      <li><a href="cv.html">CV</a></li>
       <li><a href="https://github.com/JohannesThoren">Github</a></li>
       <li><a href="https://www.instagram.com/johannes.thoren/">Instagram</a></li>
     </ul>
