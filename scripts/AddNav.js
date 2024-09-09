27bae18e82e968f8d15fc9136115c38de166486c
Johannes Thorén <Johannes@lgjt.xyz>
2024-09-09 22:41:50 +0200

    contact and about works again

--- scripts/AddNav.js
+++ scripts/AddNav.js
@@ -4,8 +4,8 @@ function AddNav() {
   nav.innerHTML = `
   <nav class="nav">
     <ul>
-      <li><a href="contact.html">Contact</a></li>
-      <li><a href="about.html">About</a></li>
+      <li><a href="/page/contact">Contact</a></li>
+      <li><a href="/page/about">About</a></li>
       <li><a href="https://github.com/JohannesThoren">Github</a></li>
       <li><a href="https://www.instagram.com/johannes.thoren/">Instagram</a></li>
     </ul>
