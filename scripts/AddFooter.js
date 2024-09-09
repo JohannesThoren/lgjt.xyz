04d9335edb7c1140c60215c2021a15cc02aab30a
Johannes Thorén <Johannes@lgjt.xyz>
2024-09-09 22:27:09 +0200

    reworking the old page, i think this one looks better

--- scripts/AddFooter.js
+++ scripts/AddFooter.js
@@ -1,5 +1,6 @@
 function AddFooter() {
   const footer = document.createElement("footer");
+  footer.className = "footer db-line"
   footer.innerHTML = `Copyright (C) 2024 Johannes Thorén`;
   document.body.appendChild(footer);
 }
--- scripts/AddNav.js
+++ scripts/AddNav.js
@@ -2,14 +2,15 @@ function AddNav() {
   // Create the nav element
   const nav = document.createElement("nav");
   nav.innerHTML = `
+  <nav class="nav">
     <ul>
       <li><a href="contact.html">Contact</a></li>
       <li><a href="about.html">About</a></li>
-      <li><a href="cv.html">CV</a></li>
       <li><a href="https://github.com/JohannesThoren">Github</a></li>
       <li><a href="https://www.instagram.com/johannes.thoren/">Instagram</a></li>
     </ul>
-    `
+  </nav>
+    `;
   // Append the nav element to the body
-  document.querySelector("Header").appendChild(nav)
+  document.querySelector("Header").appendChild(nav);
 }
