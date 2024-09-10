cb253e41ee3271dbeaba8ff227ec2c84df5083e0
Johannes Thorén <Johannes@lgjt.xyz>
2024-09-10 20:40:43 +0200

    fixed some mobile style issues

--- scripts/AddFooter.js
+++
@@ -1,6 +0,0 @@
-function AddFooter() {
-  const footer = document.createElement("footer");
-  footer.className = "footer db-line"
-  footer.innerHTML = `Copyright (C) 2024 Johannes Thorén`;
-  document.body.appendChild(footer);
-}
