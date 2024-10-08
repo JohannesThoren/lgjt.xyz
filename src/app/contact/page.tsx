53eeae676dcdfd8d3731552d432750514a93e747
Johannes Thorén <Johannes@lgjt.xyz>
2024-10-08 22:17:43 +0200

    link color fix

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -13,11 +13,11 @@ export default function Page() {
 				<p className="text-justify">
 					The only "official" way to contact me is via email. For
 					business-related inquiries, please use{" "}
-					<Link className="text-sky-300" href={"mailto:business@lgjt.xyz"}>
+					<Link className="text-sky-500" href={"mailto:business@lgjt.xyz"}>
 						business@lgjt.xyz
 					</Link>
 					. For any other contact, please use{" "}
-					<Link className="text-sky-300" href={"mailto:johannes@lgjt.xyz"}>
+					<Link className="text-sky-500" href={"mailto:johannes@lgjt.xyz"}>
 						johannes@lgjt.xyz
 					</Link>
 					. Below, you will find the public PGP keys for secure and private
