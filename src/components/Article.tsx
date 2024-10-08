3366b2a2e2112f5501ca58a1dfed3c9ca368a513
Johannes Thorén <Johannes@lgjt.xyz>
2024-10-08 22:19:30 +0200

    Article medium screen fix

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -2,7 +2,7 @@ import React, { Children, ReactNode } from "react";
 
 export default function Article(props: { title: string; children: ReactNode }) {
 	return (
-		<article className="px-5 min-w-[30dvw] lg:max-w-[35dvw]">
+		<article className="px-5 min-w-[30dvw] md:max-w-[75dvw] lg:max-w-[35dvw]">
 			<h2 className="text-3xl w-full border-solid border-b-2 dark:border-b-stone-100 border-b-stone-900 my-2">
 				<b>{props.title}</b>
 			</h2>
