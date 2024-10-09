d897d49d03cd5b95d0fdddb0f9aaca385d493ac5
Johannes Thorén <Johannes@lgjt.xyz>
2024-10-09 21:56:37 +0200

    sizing fix
    fixing some sizign,

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -2,11 +2,11 @@ import React, { ReactNode } from "react";
 
 export default function Article(props: { title: string; children: ReactNode }) {
 	return (
-		<article className="px-5 sm:min-w-[75dvw] md:min-w-[30dvw]">
+		<article className="px-5 lg:w-full lg:max-w-[40dvw] md:max-w-[80dvw]">
 			<h2 className="text-3xl w-full border-solid border-b-2 dark:border-b-stone-100 border-b-stone-900 my-2">
 				<b>{props.title}</b>
 			</h2>
 			<div className="[&>*]:my-4">{props.children}</div>
-		</article>
+		</article> 
 	);
 }
