fd5580e62ef85336dfaf2768a50c9b0a6a7ac21a
Johannes Thorén <johannes@lgjt.xyz>
2024-11-14 19:58:09 +0100

    added noreferrer and noopener to the links on the links page

--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -17,20 +17,38 @@ export default function Page() {
 			</p>
 			<section>
 				<h2 className="text-2xl">Web Precense</h2>
-				<ul className="dark:[&>li]:bg-stone-800 [&>li]:bg-stone-200 hover:[&>li]:bg-stone-300 dark:hover:[&>li]:bg-stone-700 text-center [&>li>a]:block [&>li]:rounded-md [&>li>a]:py-2 [&>li>a]:px-2 [&>li]:my-2 [&>li]:transition-colors" >
+				<ul className="dark:[&>li]:bg-stone-800 [&>li]:bg-stone-200 hover:[&>li]:bg-stone-300 dark:hover:[&>li]:bg-stone-700 text-center [&>li>a]:block [&>li]:rounded-md [&>li>a]:py-2 [&>li>a]:px-2 [&>li]:my-2 [&>li]:transition-colors">
 					<li>
-						<a href="https://github.com/JohannesThoren">Github</a>
+						<a
+							rel="noopener noreferrer"
+							href="https://github.com/JohannesThoren"
+						>
+							Github
+						</a>
 					</li>
 					<li>
-						<a href="https://www.instagram.com/johannes.thoren/">Instagram</a>
+						<a
+							rel="noopener noreferrer"
+							href="https://www.instagram.com/johannes.thoren/"
+						>
+							Instagram
+						</a>
 					</li>
 					<li>
-						<a href="https://www.linkedin.com/in/johannes-thor%C3%A9n-6a3b301a3/">
+						<a
+							rel="noopener noreferrer"
+							href="https://www.linkedin.com/in/johannes-thor%C3%A9n-6a3b301a3/"
+						>
 							Linkedin
 						</a>
 					</li>
 					<li>
-						<a href="https://discord.com/users/220112438249521156">Discord</a>
+						<a
+							rel="noopener noreferrer"
+							href="https://discord.com/users/220112438249521156"
+						>
+							Discord
+						</a>
 					</li>
 				</ul>
 			</section>
@@ -38,7 +56,9 @@ export default function Page() {
 				<h2 className="text-2xl">Projects</h2>
 				<ul className="dark:[&>li]:bg-stone-800 [&>li]:bg-stone-200 hover:[&>li]:bg-stone-300 dark:hover:[&>li]:bg-stone-700 text-center [&>li>a]:block [&>li]:rounded-md [&>li>a]:py-2 [&>li>a]:px-2 [&>li]:my-2 [&>li]:transition-colors">
 					<li>
-						<a href="https://url.lgjt.xyz">URLShortener (In Development)</a>
+						<a rel="noopener noreferrer" href="https://url.lgjt.xyz">
+							URLShortener (In Development)
+						</a>
 					</li>
 				</ul>
 			</section>
