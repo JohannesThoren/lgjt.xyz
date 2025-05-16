d85997fc07d587ed2a893c22f1965df5cbfb9678
Johannes Thorén <johannes@lgjt.xyz>
2025-05-16 20:08:06 +0200

    reading and parsing posts

---
+++ src/app/blog/getPostData.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import { readFileSync } from "fs";
+import { POST_DIR } from "../consts";
+import matter from "gray-matter";
+import { remark } from "remark";
+import html from "remark-html";
+
+export default async function getPostDat(filename: string) {
+	const fileContent = readFileSync(
+		`${process.cwd() + POST_DIR + "/" + filename}.md`,
+		"utf-8",
+	);
+
+	const matterResult = matter(fileContent);
+
+	const processedContent = await remark()
+		.use(html)
+		.process(matterResult.content);
+
+	const contentHtml = processedContent.toString();
+
+	return {
+		title: filename,
+		contentHtml,
+		...matterResult.data,
+	};
+}
