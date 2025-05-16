0e0dc1578c3d892bfb248d985bcf6826d6d15f37
Johannes Thorén <johannes@lgjt.xyz>
2025-05-16 19:00:18 +0200

    wip message

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,10 @@
 import Article from "@/components/LGJT-NEXT-COMPONENTS/Article";
+import P from "@/components/LGJT-NEXT-COMPONENTS/P";
 
 export default function Page() {
-	return <Article title={"Blog"}></Article>;
+	return (
+		<Article title={"Blog"}>
+			<P>Under Construction!</P>
+		</Article>
+	);
 }
