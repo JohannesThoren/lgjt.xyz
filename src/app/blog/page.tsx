c229cdb359a273306d32fe26fdaacc9af67d4ff0
Johannes Thorén <johannes@lgjt.xyz>
2025-05-16 18:45:47 +0200

    started on blog page

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,10 @@
+import Article from "@/components/LGJT-NEXT-COMPONENTS/Article";
+import P from "@/components/LGJT-NEXT-COMPONENTS/P";
+
+export default function Page() {
+	return (
+		<Article title={"Blog"}>
+			<P>test</P>
+		</Article>
+	);
+}
